e442bda482c59d1f4abc7e3cee1de64c73bc4efe
chaknogf <chaknogf@gmail.com>
2025-05-24 13:03:00 -0600

    feat: /inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { DashboardComponent } from './principal/dashboard/dashboard.component';
 import { HomeComponent } from './principal/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'medicapp', pathMatch: 'full' },
-  { path: 'medicapp', component: HomeComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: HomeComponent },
   { path: 'dash', component: DashboardComponent },
 ];
