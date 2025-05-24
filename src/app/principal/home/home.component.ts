a3c4049f2c931d7414a33e2f79125f16ea222453
chaknogf <chaknogf@gmail.com>
2025-05-24 12:59:46 -0600

    feat: assets svg

--- src/app/principal/home/home.component.ts
+++ src/app/principal/home/home.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import { SvgHomeComponent } from "../svg-home/svg-home.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, LoginComponent, SvgHomeComponent],
 })
 export class HomeComponent implements OnInit {
 
---
+++ src/app/principal/svg-home/svg-home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-home',
+  templateUrl: './svg-home.component.html',
+  styleUrls: ['./svg-home.component.css'],
+  standalone: true
+})
+export class SvgHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
