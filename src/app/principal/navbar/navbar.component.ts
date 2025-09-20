37fa066359fcb21ac9474d65fa2f9945c8b1f4ab
chaknogf <chaknogf@gmail.com>
2025-09-20 11:13:37 -0600

    style: navbar style

--- src/app/principal/navbar/navbar.component.ts
+++ src/app/principal/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
+import { logoicon2, logoicon } from './../../shared/icons/svg-icon';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-navbar',
@@ -11,8 +14,20 @@ export class NavbarComponent {
   @Input() usuario: string = 'usuario';
   @Input() rol: string = 'rol';
   @Output() cerrarSesion = new EventEmitter<void>();
+  private sanitizarSvg(svg: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(svg);
+  }
 
-  constructor(private router: Router) { }
+  logoicon: SafeHtml = logoicon
+  logoicon2: SafeHtml = logoicon2
+
+  constructor(
+    private router: Router,
+    private sanitizer: DomSanitizer
+  ) {
+    this.logoicon = this.sanitizarSvg(logoicon);
+    this.logoicon2 = this.sanitizarSvg(logoicon2);
+  }
 
   logout() {
     this.cerrarSesion.emit();
